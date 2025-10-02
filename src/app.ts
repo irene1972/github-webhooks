6ba87abcb7e4ed4ee73dc4053c8d5d52d4f3e2a7
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-02 17:22:43 +0200

    primer commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { envs } from './config';
+
+(()=>{
+    main();
+})();
+
+
+function main(){
+    const app = express();
+    
+    app.post('/api/github', (req, res) => {
+        res.json('Github endpoint')
+    });
+    
+    app.listen(envs.PORT,()=>{
+        console.log(`App running on port ${envs.PORT}`);
+    });
+}
